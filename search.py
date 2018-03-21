f8860196acccd955b5d24fee9fae39c3f3657165
root <xmuthad@163.com>
2018-03-21 15:12:37 +0800

    init elastic

--- search.py
+++ search.py
@@ -3,5 +3,13 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-doc = {
-    'author':}
+"""
+hosts=["host1", "host2"]
+maxsize is the connnection to each node.By default
+we allow urllib3 to open up to 10 connections to each node.
+
+"""
+#es = Elasticsearch(hosts,
+#                   maxsize=30,
+#                   http_compress=True)
+res = es.mget()
