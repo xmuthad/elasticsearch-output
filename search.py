920b90929a7d6f5956da7365e66e68e6fe2b73b6
holger <xmuthad@163.com>
2018-03-22 14:53:52 +0800

    add count

--- search.py
+++ search.py
@@ -10,37 +10,56 @@ maxsize is the connnection to each node.By default
 we allow urllib3 to open up to 10 connections to each node.
 
 """
-ES = None
 LIMIT = 10000
-def get_elastic():
-    global ES
-    if not ES:
-        ES = Elasticsearch(hosts,
-                           http_compress=True)
-    return ES
 
-def multi_get():
-    es = get_elastic()
-    #health_status = es.cluster.health()
-    #print health_status
-    #res = es.mget(params)
-    #body = {"query":{"term":{}}}
 
-    #number = es.count(body=body) 
-    from_ = 0
-    body = """
-        {"index":["log-2018.03.21"]}
-        {"query":{"match_all":{}},"from":%(from_)d, "size":%(limit)d}
-        """ % dict(from_=from_, limit=LIMIT)
-    res = es.msearch(body, doc_type='message')
-    total = res['responses'][0]['hits']['total']
-    hits = res['responses'][0]['hits']['hits']
-    for i in xrange(total/LIMIT):
+class Search(object):
+
+    def __init__(self):
+        self.es = Elasticsearch(hosts,
+                               http_compress=True)
+    
+    def multi_get(self):
+        #health_status = es.cluster.health()
+        #print health_status
+        #res = es.mget(params)
+        #body = {"query":{"term":{}}}
+    
+        #number = es.count(body=body) 
+        index = ["log-2018.03.21"] 
+        from_ = 0
         body = """
-            {"index":["log-2018.03.21"]}
-            {"query":{"match_all":{}},"from":LIMIT*(1+i), "size":LIMIT}"""
-        res = es.msearch(body, doc_type='message')
-        hits.append(hits)
-    print sys.getsizeof(hits)
-    print len(hits)
-multi_get()
+            {"index":%(index)s}
+            {"query":{"match_all":{}},"from":%(from_)d, "size":%(limit)d}
+            """ % dict(index=index, from_=from_, limit=LIMIT)
+        res = self.es.msearch(body, doc_type='message')
+        total = res['responses'][0]['hits']['total']
+        hits = res['responses'][0]['hits']['hits']
+        for i in xrange(total/LIMIT):
+            body = """
+                {"index":["log-2018.03.21"]}
+                {"query":{"match_all":{}},"from":LIMIT*(1+i), "size":LIMIT}"""
+            res = es.msearch(body, doc_type='message')
+            hits.append(hits)
+        return hits
+    
+    def _count(self, index=None, item=None, value=None):
+        body = {
+            "query":{
+                "term": {
+                    item: value,
+                }
+            } 
+        }
+        res = self.es.count(index=index, body=body)
+        return res['count']
+
+class LogReport(Search):
+     def __init__(self):
+         super(LogReport, self).__init__()
+     def converge_info(self):
+         return self._count()
+
+reporting = LogReport() 
+end = reporting.converge_info()
+print end
